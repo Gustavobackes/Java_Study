d7ac224b372d5f6f935076de46c0fe3c61826fe0
gustavobackes <gustavo.kitzberger@projuris.com.br>
2022-07-12 14:54:36 -0300

    commit

--- AulaJPA/src/main/java/com/example/aulajpa/entities/User.java
+++ AulaJPA/src/main/java/com/example/aulajpa/entities/User.java
@@ -12,7 +12,6 @@ public class User {
     private String email;
     private Double salary;
 
-
     public User() {
     }
 
--- AulaJPA/src/main/java/com/example/aulajpa/repositories/UserRepository.java
+++ AulaJPA/src/main/java/com/example/aulajpa/repositories/UserRepository.java
@@ -1,8 +1,13 @@
 package com.example.aulajpa.repositories;
 
 import com.example.aulajpa.entities.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    Page<User> findBySalaryBetween(Double minSalary, Double maxSalary, Pageable pageable);
+
+    Page<User> findByNameContainingIgnoreCase(String name, Pageable pageable);
 }
